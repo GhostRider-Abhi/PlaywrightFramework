937cc8496bafe952acdf11fc645eff013ea085cf
GhostRider-Abhi <55529665+GhostRider-Abhi@users.noreply.github.com>
2024-01-15 16:48:15 +0530

    adding playwright yml file (#1)
    * adding playwright yml file
    
    * adding timout for visibility of an element
    
    * adding timout in the playwright config
    
    * updating the assertions
    
    * reverting the assertion for visibility

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,6 +15,10 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 30 * 1000,
+  },
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
