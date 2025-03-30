53296a8e2923322907fab9986509d3da726b977f
GhostRider-Abhi <bhattabhi1289@gmail.com>
2025-03-30 19:04:41 +0530

    adding waiting time

--- tests/research/naukriProfile.spec.ts
+++ tests/research/naukriProfile.spec.ts
@@ -12,6 +12,7 @@ testData.forEach(data => {
   test(`naukri profile ${data.name}`, async ({ page }) => {
     await page.goto('https://www.naukri.com/nlogin/login');
     await page.fill('#usernameField', data.email);
+    await page.waitForTimeout(2000);
     await page.locator('#passwordField').fill(data.password);
     await page.getByRole('button', { name: 'Login', exact: true }).click();
     await page.waitForLoadState('load');
